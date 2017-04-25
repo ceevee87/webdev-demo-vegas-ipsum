bf75db6087c3f9af94722b2bd60b1d963d0ce470
John Ramirez <cyclopsvision87@icloud.com>
2017-04-25 11:12:17 -0600

    initial version.

---
+++ src/main/java/com/cvcoding/vegasipsum/LoremIpsum.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cvcoding.vegasipsum;
+
+import java.util.List;
+
+/**
+ *
+ * @author johnr
+ */
+public interface LoremIpsum {
+    
+    public void shuffle();
+    
+    // return a bunch of words but formulated as a single string.
+    public String getWords(int min, int max);
+    
+    // return a list paragraphs where each paragraph is a string.
+    public List<String> getParagraphs(int min, int max);
+    
+    // the default version of LoremIpsum should load in the latin
+    // words already. the functions below allow you to augment this
+    // vocabulary with your own fun words.
+    // If you want "hi-ho silver" to be in your lorem but to appear
+    // more frequently than average set count to some number > 10.
+    public void addWords(String phrase);
+    public void addWords(String phrase, int count);
+    public void addWords(List<String> phrases);
+    public void addWords(List<String> phrases, int count);
+
+    // configure how many sentences per paragraph
+    public void setParagraphSizes(int min, int max);
+    
+}
---
+++ src/main/java/com/cvcoding/vegasipsum/VegasIpsum.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cvcoding.vegasipsum;
+
+import java.util.List;
+
+/**
+ *
+ * @author johnr
+ */
+public class VegasIpsum implements LoremIpsum {
+
+    @Override
+    public void shuffle() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getWords(int min, int max) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<String> getParagraphs(int min, int max) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addWords(String phrase) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addWords(String phrase, int count) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addWords(List<String> phrases) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addWords(List<String> phrases, int count) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setParagraphSizes(int min, int max) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
